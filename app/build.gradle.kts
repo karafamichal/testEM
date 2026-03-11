93181b639d799c5125382a426dc9d7df1237b0e5
Michal Karafa <michal400karafa@gmail.com>
2026-03-11 19:26:42 +0100

    Changed version tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.5.5"
+        versionName = "1.5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
